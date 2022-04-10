b4526f6ab282b7b91b2d90ba5a69018b505502fe
cgx <601051903@qq.com>
2022-04-10 17:13:09 +0800

    feat：添加动画

--- src/layout.vue
+++ src/layout.vue
@@ -29,7 +29,7 @@ export default {
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  border: 1px solid red;
+
   &.hasSide {
     flex-direction: row;
   }
--- src/side.vue
+++ src/side.vue
@@ -1,14 +1,38 @@
 <template>
-  <div class="side"><slot></slot></div>
+  <transition name="slide">
+    <div class="side" v-if="visible">
+      <slot></slot>
+      <button @click="visible = false">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'GuluSide',
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
 .side {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.3s;
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  margin-left: -200px;
 }
 </style>
