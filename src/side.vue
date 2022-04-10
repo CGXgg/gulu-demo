18ee25139bb0d813c646aca3c957b8fda894cabf
cgx <601051903@qq.com>
2022-04-10 16:08:18 +0800

    feat:基本 功能完成

---
+++ src/side.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="side"><slot></slot></div>
+</template>
+
+<script>
+export default {
+  name: 'GuluSide',
+};
+</script>
+
+<style lang="scss" scoped>
+.side {
+}
+</style>
