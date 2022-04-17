f4a53ba57454917939166bcc05a9ee33a15f9638
cgx <601051903@qq.com>
2022-04-17 12:53:13 +0800

    feat:可以设置默认selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -15,6 +15,10 @@ export default {
       type: String,
       required: true,
     },
+    name: {
+      type: String,
+      required: true,
+    },
   },
   data() {
     return {
@@ -24,9 +28,11 @@ export default {
   inject: ['eventBus'],
   mounted() {
     this.eventBus &&
-      this.eventBus.$on('update:selected', (vm) => {
-        if (vm !== this) {
+      this.eventBus.$on('update:selected', (name) => {
+        if (name !== this.name) {
           this.close();
+        } else {
+          this.show();
         }
       });
   },
@@ -35,13 +41,15 @@ export default {
       if (this.open) {
         this.open = false;
       } else {
-        this.open = true;
-        this.eventBus && this.eventBus.$emit('update:selected', this);
+        this.eventBus && this.eventBus.$emit('update:selected', this.name);
       }
     },
 
     close() {
       this.open = false;
+    },
+    show() {
+      this.open = true;
     },
   },
 };
